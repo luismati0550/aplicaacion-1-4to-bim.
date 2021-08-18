8a19fc75ddd271bd491c0e679ccdcd44f4ed060d
luismati0550 <87058945+luismati0550@users.noreply.github.com>
2021-08-17 23:23:26 -0600

    Add files via upload

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string R = "s";
+            while (R == "s")
+            {
+                Console.WriteLine("Escribe el nombre del arhivo");
+                string archivo = Console.ReadLine();
+
+
+
+                StreamWriter ar;
+                ar = File.CreateText(archivo + ".txt");
+                ar.WriteLine("Archivo Creado");
+                ar.Close();
+
+
+
+                Console.WriteLine("Quieres crear otro archivo?");
+                Console.WriteLine("s/n");
+                R = Console.ReadLine();
+                Console.Clear();
+                if (R == "no")
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
